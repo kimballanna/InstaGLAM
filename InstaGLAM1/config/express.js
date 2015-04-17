a265fac701420d0b5ac85c7e3ea57463250382ff
Anna Kimball <kimball.anna@knights.ucf.edu>
2015-04-16 21:30:25 -0400

    Realtime notification

--- InstaGLAM1/config/express.js
+++ InstaGLAM1/config/express.js
@@ -6,13 +6,12 @@
 var fs = require('fs'),
 	http = require('http'),
 	https = require('https'),
-	socketio = require('socket.io'),
 	express = require('express'),
+	socketio = require('socket.io'),
 	morgan = require('morgan'),
-	logger = require('./logger'),
 	bodyParser = require('body-parser'),
 	session = require('express-session'),
-	compression = require('compression'),
+	compress = require('compression'),
 	methodOverride = require('method-override'),
 	cookieParser = require('cookie-parser'),
 	helmet = require('helmet'),
@@ -49,13 +48,11 @@ module.exports = function(db) {
 	});
 
 	// Should be placed before express.static
-	app.use(compression({
-		// only compress files for the following content types
+	app.use(compress({
 		filter: function(req, res) {
 			return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
 		},
-		// zlib option for compression level
-		level: 3
+		level: 9
 	}));
 
 	// Showing stack errors
@@ -68,11 +65,11 @@ module.exports = function(db) {
 	app.set('view engine', 'server.view.html');
 	app.set('views', './app/views');
 
-	// Enable logger (morgan)
-	app.use(morgan(logger.getLogFormat(), logger.getLogOptions()));
-
 	// Environment dependent middleware
 	if (process.env.NODE_ENV === 'development') {
+		// Enable logger (morgan)
+		app.use(morgan('dev'));
+
 		// Disable views cache
 		app.set('view cache', false);
 	} else if (process.env.NODE_ENV === 'production') {
@@ -86,16 +83,6 @@ module.exports = function(db) {
 	app.use(bodyParser.json());
 	app.use(methodOverride());
 
-	// Use helmet to secure Express headers
-	app.use(helmet.xframe());
-	app.use(helmet.xssFilter());
-	app.use(helmet.nosniff());
-	app.use(helmet.ienoopen());
-	app.disable('x-powered-by');
-
-	// Setting the app router and static folder
-	app.use(express.static(path.resolve('./public')));
-
 	// CookieParser should be above session
 	app.use(cookieParser());
 
@@ -107,9 +94,7 @@ module.exports = function(db) {
 		store: new mongoStore({
 			db: db.connection.db,
 			collection: config.sessionCollection
-		}),
-		cookie: config.sessionCookie,
-		name: config.sessionName
+		})
 	}));
 
 	// use passport session
@@ -118,6 +103,16 @@ module.exports = function(db) {
 
 	// connect flash for flash messages
 	app.use(flash());
+
+	// Use helmet to secure Express headers
+	app.use(helmet.xframe());
+	app.use(helmet.xssFilter());
+	app.use(helmet.nosniff());
+	app.use(helmet.ienoopen());
+	app.disable('x-powered-by');
+
+	// Setting the app router and static folder
+	app.use(express.static(path.resolve('./public')));
 
 	// Globbing routing files
 	config.getGlobbedFiles('./app/routes/**/*.js').forEach(function(routePath) {
@@ -147,6 +142,9 @@ module.exports = function(db) {
 	});
 
 	if (process.env.NODE_ENV === 'secure') {
+		// Log SSL usage
+		console.log('Securely using https protocol');
+
 		// Load SSL key and certificate
 		var privateKey = fs.readFileSync('./config/sslcerts/key.pem', 'utf8');
 		var certificate = fs.readFileSync('./config/sslcerts/cert.pem', 'utf8');
@@ -160,15 +158,11 @@ module.exports = function(db) {
 		// Return HTTPS server instance
 		return httpsServer;
 	}
-
-	// Attach Socket.io
-	
-	
+	// Use Socket.io
 	var server = http.createServer(app);
 	var io = socketio.listen(server);
 	app.set('socketio', io);
 	app.set('server', server);
-	
 	// Return Express server instance
 	return app;
 };
--- InstaGLAM1/server.js
+++ InstaGLAM1/server.js
@@ -13,28 +13,12 @@ var init = require('./config/init')(),
  */
 
 // Bootstrap db connection
-//Original setup
-var db = mongoose.connect(config.db.uri, config.db.options, function(err) {
-	if (err) {
-		console.error(chalk.red('Could not connect to MongoDB!'));
-		console.log(chalk.red(err));
-	}
-}); 
-//New setup
-/*
 var db = mongoose.connect(config.db, function(err) {
 	if (err) {
 		console.error(chalk.red('Could not connect to MongoDB!'));
 		console.log(chalk.red(err));
 	}
-});*/
-
-/*
-mongoose.connection.on('error', function(err) {
-	console.error(chalk.red('MongoDB connection error: ' + err));
-	process.exit(-1);
-	}
-);*/
+});
 
 // Init the express application
 var app = require('./config/express')(db);
@@ -43,9 +27,7 @@ var app = require('./config/express')(db);
 require('./config/passport')();
 
 // Start the app by listening on <port>
-//Original
-//app.listen(config.port);
-//For socketsio, but crashes site
+// Modified for socket.io
 app.get('server').listen(config.port);
 
 // Expose app
@@ -53,12 +35,3 @@ exports = module.exports = app;
 
 // Logging initialization
 console.log('MEAN.JS application started on port ' + config.port);
-console.log('--');
-console.log(chalk.green(config.app.title + ' application started'));
-console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
-console.log(chalk.green('Port:\t\t\t\t' + config.port));
-console.log(chalk.green('Database:\t\t\t' + config.db.uri));
-if (process.env.NODE_ENV === 'secure') {
-	console.log(chalk.green('HTTPs:\t\t\t\ton'));
-}
-console.log('--');
